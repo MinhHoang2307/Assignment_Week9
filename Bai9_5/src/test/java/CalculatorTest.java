2d622c8cb2ee930b1ffcbdea5d4790180f242c19
MinhHoang2307 <25023478@vnu.edu.vn>
2026-04-28 22:00:32 +0700

    bai5

--- Bai9_5/src/test/java/CalculatorTest.java
+++ Bai9_5/src/test/java/CalculatorTest.java
@@ -1,23 +1,11 @@
-import org.example.Calculator;
-import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.testng.AssertJUnit.assertEquals;
+// Xóa hết TestNG, chỉ dùng JUnit 5
+import org.example.Calculator;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
-    @Test
-    public void testAdd() {
-        Calculator calc = new Calculator();
-        int result = calc.add(2,3);
-        assertEquals(5,result);
-    }
 
-    @Test
-    void testSubtract() {
-        Calculator calc = new Calculator();
-        assertEquals(1, calc.subtract(3, 2));
-    }
     @Test
     void testAll() {
         Calculator calc = new Calculator();
@@ -26,6 +14,4 @@ public class CalculatorTest {
         assertEquals(6, calc.multiply(2, 3));
         assertEquals(2, calc.divide(4, 2));
     }
-// Viết thêm cho đến khi hết các hàm trong Calculator
-
 }
