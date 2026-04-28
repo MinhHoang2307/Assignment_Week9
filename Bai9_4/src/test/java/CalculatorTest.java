a06ec5051ce4eff57fd99c55ebd0e6f6716d970e
MinhHoang2307 <25023478@vnu.edu.vn>
2026-04-28 20:59:38 +0700

    Hoàn thiện cấu trúc thư mục từ Bài 1 đến Bài 4 và cấu hình Matrix CI

--- Bai9_4/src/test/java/CalculatorTest.java
+++ Bai9_4/src/test/java/CalculatorTest.java
@@ -1,6 +1,7 @@
 import org.example.Calculator;
 import org.testng.annotations.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.testng.AssertJUnit.assertEquals;
 
 public class CalculatorTest {
@@ -9,5 +10,11 @@ public class CalculatorTest {
         Calculator calc = new Calculator();
         int result = calc.add(2,3);
         assertEquals(5,result);
+    }
+    @Test
+    void testOSSpecificPath() {
+        // Dùng dấu gạch chéo ngược (Windows) để cố tình gây lỗi trên Linux/Mac
+        String path = "target\\test-output\\result.txt";
+        assertNotNull(path);
     }
 }
